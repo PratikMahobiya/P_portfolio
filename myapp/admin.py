7e4683265d05f3ed621ae863302fe79cc0551e64
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-12-13 16:54:40 +0530

    UPDATE:-

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.Contact_form_model)
-admin.site.register(models.File_model)
+@admin.register(models.Contact_form_model)
+class Contact_admin(admin.ModelAdmin):
+    list_display = ('U_id','Name','Email','Contact','Message','Date','File_status')
+    list_per_page = 10
+    search_fields = ['Name','Contact']
+
+@admin.register(models.File_model)
+class File_admin(admin.ModelAdmin):
+    list_display = ('U','File')
+    list_per_page = 10
