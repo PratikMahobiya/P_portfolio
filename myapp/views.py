bdb73e8a3b77012b9037567ebcae5022507109c5
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-12-13 14:00:38 +0530

    UPDATE:- INBOX SMS, CONTENT OF WEBSITE

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,11 @@
+import requests
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
 from . import forms
 from . import models
 
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from datetime import datetime
 from twilio.rest import Client
 
@@ -66,3 +69,16 @@ def send_whatsapp_message(Name,Email,Contact,Message,File_status):
 	                     from_='whatsapp:+14155238886',
 	                     to='whatsapp:+917000681073'
 	                 )
+
+@receiver(post_save,sender=models.Contact_form_model)
+def send_sms(sender, created, **kwargs):
+    obj = kwargs['instance']
+    if created:
+      if obj.mobile != 0:
+        # Sending SMS
+        sms_content = "Hello Pratik,\n\nSomeone trying to contact you.\n\nName: {}\nContact: {}\nEmail: {}\nFile_status: {}\n\nMessage: {}".format(obj.Name,obj.Contact,obj.Email,obj.File_status,obj.Message)
+        my_data = {'sender_id': 'FST2SMS','message': sms_content,'language': 'english','route': 'p','numbers': obj.mobile}
+        headers = {'authorization': '6a0iXHGODBECvnVbmSoeYPd5K1Mgl3thUL2zNQp79cJWRfTZFx40eYPvV2SJ1lKXU9Tzp8qGtCsDcuL5',\
+                    'Content-Type': "application/x-www-form-urlencoded",'Cache-Control': "no-cache"}
+        url = "https://www.fast2sms.com/dev/bulk"
+        requests.request("POST",url,data = my_data,headers = headers)
